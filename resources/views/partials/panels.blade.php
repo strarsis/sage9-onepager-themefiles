94ec8f8bad9d5dead798a7de3a2dda24e5adf566
strarsis <strarsis@gmail.com>
2018-02-27 21:44:13 +0100

    Update panels.blade.php

--- resources/views/partials/panels.blade.php
+++ resources/views/partials/panels.blade.php
@@ -4,15 +4,13 @@
     {{--
      * Filter number of front page sections in Twenty Seventeen.
      *
-     * @since Twenty Seventeen 1.0
-     *
      * @param int $num_sections Number of front page sections.
      --}}
     @php
       $num_sections = apply_filters( 'onepager_front_page_sections', 4 );
       global $onepagercounter;
     @endphp
-    {{-- Create a setting and control for each of the sections available in the theme. --}}
+
     @for ( $i = 1; $i < ( 1 + $num_sections ); $i++ )
       @php $onepagercounter = $i; @endphp
       {!! \strarsis\Sage9Onepager\Controls::front_page_section( null, $i ) !!}

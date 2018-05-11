7934c813e1cbff3610c6f4537caf91dd1ce55c6d
strarsis <strarsis@gmail.com>
2018-05-11 19:06:13 +0200

    Update front-page.php

--- resources/views/front-page.php
+++ resources/views/front-page.php
@@ -1,9 +1,16 @@
+@php
+  $frontpage_id   = get_option( 'page_on_front' );
+  $frontpage_slug = get_post_field( 'post_name', $frontpage_id );
+@endphp
 @extends('layouts.app')
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @include('partials.content-page')
+
+    <div class="home" id="{{ $frontpage_slug }}">
+      @include('partials.page-header')
+      @include('partials.content-page')
+    </div>
 
     @include('partials.panels')
   @endwhile

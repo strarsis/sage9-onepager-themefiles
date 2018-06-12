f273f8034753e655f3751f461a1656003b4bc702
strarsis <strarsis@gmail.com>
2018-06-12 19:23:12 +0200

    Update single-panels.blade.php

--- resources/views/single-panels.blade.php
+++ resources/views/single-panels.blade.php
@@ -7,6 +7,8 @@
 <article id="{{ $post->post_name }}" {!! post_class( 'onepager-panel ' ) !!} >
   <div class="panel-content">
     @include('partials.page-header')
+    @php \strarsis\Sage9Onepager\Controls::edit_link( get_the_ID() ) @endphp
+
     @include('partials.content-page')
   </div><!-- .panel-content -->
 </article><!-- #post-## -->
